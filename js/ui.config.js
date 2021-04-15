3189f4dfc341ff7f587149f7ee6ea3b97c2cd1c8
Melnychuk-27 <79938449+Melnychuk-27@users.noreply.github.com>
2021-04-15 14:16:01 +0300

    Delete js directory

--- js/ui.config.js
+++
@@ -1,8 +0,0 @@
-const UI = {
-    form: document.forms['advice__form'],
-    inputEmail: document.forms["advice__form"]["email"],
-    inputTell: document.forms["advice__form"]["tell"],
-    inputNAme: document.forms["advice__form"]["name"]
-}
-
-export default UI;
